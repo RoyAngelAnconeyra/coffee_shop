c55764bfd69ca724e6f7d20bfd1814b79f3952a7
Angel Anconeyra <angel1706.1308@gmail.com>
2026-03-15 16:32:46 -0500

    Segundo commit

--- products/models.py
+++ products/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Product(models.Model):
     name = models.TextField(max_length=200, verbose_name='nombre')
-    description = models.TextField(max_length=530, verbose_name='descripción')
+    description = models.TextField(max_length=300, verbose_name='descripción')
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='precio')
     available = models.BooleanField(default=True, verbose_name='disponible')
     photo = models.ImageField(upload_to='logos', null=True, blank=True, verbose_name='foto')
